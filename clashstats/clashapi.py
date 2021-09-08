3ec777625f5b170c92b11c5a5f9ecee0fabb9b33
ryand3031 <ryand3031@gmail.com>
2021-09-08 16:16:06 -0700

    First Commit

---
+++ clashstats/clashapi.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+#ADD clash API key here
+key = "ADD_KEY_HERE"
+
+
+def get_player_info(pid):
+    r=requests.get( f"https://api.clashroyale.com/v1/players/%23{pid}", headers={"Accept":"application/json", "authorization":f"Bearer {key}"}, params = {"limit":20})
+    #print(json.dumps(r.json(), indent = 2))
+    return r.json()
+
+def get_player_chests(pid):
+    r=requests.get( f"https://api.clashroyale.com/v1/players/%23{pid}/upcomingchests", headers={"Accept":"application/json", "authorization":f"Bearer {key}"}, params = {"limit":20})
+    return r.json()
+
+
+def addWinRate(playerInfo):
+    totalBattles = (playerInfo['wins']+playerInfo['losses'])
+    playerInfo['winRate'] = round((playerInfo['wins']/totalBattles)*100,2)
+    playerInfo['threeCrownRate'] = round(playerInfo['threeCrownWins']/totalBattles*100, 2)
+
+# playid = 'YQJVLLY8'
+# r=requests.get(f"https://api.clashroyale.com/v1/players/%23{playid}", headers={"Accept":"application/json", "authorization":f"Bearer {key}"}, params = {"limit":20})
+# print(r.json()['name'])
---
+++ clashstats/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from . import clashapi
+#'#YQJVLLY8'
+
+
+def home_page(request):
+    return render(request, 'home.html')
+
+def player_page(request, player_id):
+    
+    playerInfo = clashapi.get_player_info(player_id)
+    print(playerInfo['name'])
+    contex = {
+        'pid': player_id
+    }
+    return render(request, 'player.html', playerInfo)
+
+def search(request):
+    if request.method == 'GET':
+        pid = request.GET['pid'].upper()
+        #print(pid[1:])
+        if(pid[0]=='#'):
+            pid=pid[1:]
+        playerInfo = clashapi.get_player_info(pid)
+        #print(playerInfo)
+        if('tag' in playerInfo):
+            clashapi.addWinRate(playerInfo)
+            chestInfo = clashapi.get_player_chests(pid)
+            #print(chestInfo)
+            playerInfo['chestInfo'] = chestInfo
+            print(playerInfo['chestInfo'])
+            return render(request, 'player.html', playerInfo)
+    return render(request, 'notfound.html')
+
+# 'YQJVLLY8'
